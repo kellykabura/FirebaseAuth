25cfcaa61ae890193b3fb6d7c6c6f807ab79baad
Kelly <kellykk@kellys-MacBook-Air.local>
2023-09-06 12:19:45 +0300

    initial commit

---
+++ app/src/main/java/com/example/firebaseauth/navigation/Routes.kt
@@ -0,0 +1,5 @@
+package com.example.firebaseauth.navigation
+
+const val ROUTE_LOGIN="login"
+const val ROUTE_REGISTER="register"
+
---
+++ app/src/main/java/com/example/firebaseauth/ui/theme/screens/ggg.kt
@@ -0,0 +1,2 @@
+package com.example.firebaseauth.ui.theme.screens
+
